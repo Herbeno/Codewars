bcbc69353adb210a7ccdce51fc7244d2c1a98bb8
Herben <hrbdata17@gmail.com>
2023-06-14 20:49:22 -0300

    super

--- 8kyu/Super Duper Easy.py
+++ 8kyu/Super Duper Easy.py
@@ -1,3 +1,7 @@
  # Make a function that returns the value multiplied by 50 and increased by 6. if the value entered is a string shold return "Error".
-def multiply(f):
-    value = (f * 50) + 6
+def problem(a):
+    if (type(a)) == int or (type(a)) == float:
+        f = (a * 50) + 6   
+        return f
+    else:
+        return "Error"
