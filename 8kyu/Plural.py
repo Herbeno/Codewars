93a98185dec173ebc42cd3c52f6dca1065f91538
Herben <hrbdata17@gmail.com>
2023-06-15 06:36:33 -0300

    Create Plural.py

---
+++ 8kyu/Plural.py
@@ -0,0 +1,4 @@
+# We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not.
+
+def plural(n):
+    return n != 1
