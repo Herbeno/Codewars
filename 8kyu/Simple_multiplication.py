793958ad17db3c552ca3a10aba8b5572bc2763c6
Herben <hrbdata17@gmail.com>
2023-06-14 21:08:16 -0300

    xor

--- 8kyu/Simple_multiplication.py
+++ 8kyu/Simple_multiplication.py
@@ -5,6 +5,6 @@ def simple_multiplication(number) :
         simple_multiplication = number * 8
         return simple_multiplication
     # No código acima, colocamos uma codição de que se o resto da divisão do 'number' por 2 for igual a 0(ou seja, o número é par), ele será multiplicado por 8.
-    # Logo, a próxima possibilidade que teremos é de números ímpares, que foram os que restaram
+    # Logo, a próxima possibilidade que teremos é de números ímpares, que foram os que restaram, e seram multiplicados por 9.
     simple_multiplication = number * 9
     return simple_multiplication
